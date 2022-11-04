1257f5d713daf26289ade12759853ac4b6d75042
Yu Wang <70514121+wangyu-sd@users.noreply.github.com>
2022-11-04 13:41:51 +0800

    Create train_for_ustpto.sh

---
+++ scripts/train_for_ustpto.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python entry.py\
+  --batch_size 512 \
+  --acc_batches 1 \
+  --d_model 128 \
+  --dim_feedforward 256 \
+  --gpus 1\
+  --epochs 2000 \
+  --dropout 0.2 \
+  --warmup_updates 2000 \
+  --tot_updates 1000000 \
+  --dataset data/USPTO50K_aug \
+  --known_rxn_type \
+  --norm_first \
+  --nhead 32 \
+  --num_shared_layer 6 \
+  --num_rc_layer 0 \
+  --num_lg_layer 6 \
+  --num_ct_layer 6 \
+  --num_h_layer 6 \
+  --seed 123 \
+  --cuda 2 \
+  --max_ct_atom 4 \
+  --max_single_hop 4
